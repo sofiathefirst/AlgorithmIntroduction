8551862feb645662cebd4a71b73248aa5019dd12
zhangyumei <zhangyumei@ctirobot.com>
2018-05-24 11:15:39 +0800

    cpp

---
+++ g.sh
@@ -0,0 +1,7 @@
+echo "# AlgorithmIntroduction" >> README.md
+git init
+git add *.c 
+git add *.sh
+git commit -m "first commit"
+git remote add origin git@github.com:sofiathefirst/AlgorithmIntroduction.git
+git push -u origin master
